b40cde199f7e7092b85d7a5922fc2f178e894a3c
LucaDiStasio <luca.distasio@gmail.com>
2017-07-19 18:28:55 +0200

    commit on 2017-07-19 18:27

--- matlab/RVE-generation/writeABQfiber.m
+++ matlab/RVE-generation/writeABQfiber.m
@@ -1,23 +1,23 @@
-function[fiber]=writeABQfiber(inpfileFullPath,logfilefullpath,fiber)
+function[fiber,Nlast]=writeABQfiber(inpfileFullPath,logfilefullpath,fiber,N0,isCohesive)
 %%
 %==============================================================================
-% Copyright (c) 2016 Université de Lorraine & Luleå tekniska universitet
+% Copyright (c) 2016 Universitï¿½ de Lorraine & Luleï¿½ tekniska universitet
 % Author: Luca Di Stasio <luca.distasio@gmail.com>
 %                        <luca.distasio@ingpec.eu>
 %
 % Redistribution and use in source and binary forms, with or without
 % modification, are permitted provided that the following conditions are met:
-% 
-% 
+%
+%
 % Redistributions of source code must retain the above copyright
 % notice, this list of conditions and the following disclaimer.
 % Redistributions in binary form must reproduce the above copyright
 % notice, this list of conditions and the following disclaimer in
 % the documentation and/or other materials provided with the distribution
-% Neither the name of the Université de Lorraine or Luleå tekniska universitet
+% Neither the name of the Universitï¿½ de Lorraine or Luleï¿½ tekniska universitet
 % nor the names of its contributors may be used to endorse or promote products
 % derived from this software without specific prior written permission.
-% 
+%
 % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 % AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 % IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -32,11 +32,14 @@ function[fiber]=writeABQfiber(inpfileFullPath,logfilefullpath,fiber)
 %==============================================================================
 %
 %  DESCRIPTION
-%  
+%
 %  A function to generate the mesh of a single fiber with surrounding ring
 %  of matrix and write it to file
 %
-%  Output: 
+%  N0 is the node label of the first node
+%  Nlast is the node label of the last node written to file
+%
+%  Output:
 %
 %%
 

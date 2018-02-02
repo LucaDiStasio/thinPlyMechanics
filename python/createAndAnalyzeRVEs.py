29faf4d919240182731414f032daffce539cc568
LucaDiStasio <luca.distasio@gmail.com>
2018-02-02 14:32:13 +0100

    commit on 2018-02-02 at 14:32:12

--- python/createAndAnalyzeRVEs.py
+++ python/createAndAnalyzeRVEs.py
@@ -3091,9 +3091,9 @@ def main(argv):
                                      'local':{'directory':[],
                                                'filenames':{'Jintegral':[],
                                                             'stressesatboundary':[],
-                                                            'crackdisplacements':[]}}
+                                                            'crackdisplacements':[]}}}
                            'sql':{'global':{'directory':'D:/OneDrive/01_Luca/07_DocMASE/07_Data/03_FEM/caePythonTest',
-                                            'filename':'caePythonTestDB'}}}
+                                            'filename':'caePythonTestDB'}}
                           }
     RVEparams['solver'] = {'cpus':12}
 

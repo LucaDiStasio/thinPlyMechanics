bb74fd8f7cc32f740978416d074007ca557dba6a
LucaDiStasio <luca.distasio@gmail.com>
2018-03-12 08:12:05 +0100

    commit on 12/03/2018 at 08:12:05

---
+++ windows-cmd/reportData.cmd
@@ -0,0 +1,1 @@
+cd D:/01*/07*/03*/Curved* && python D:/01_Luca/06_WD/thinPlyMechanics/python/reportData.py -w D:/OneDrive/01_Luca/07_DocMASE/07_Data/03_FEM/sweepOverDeltathetaL28 -i 2018-03-08_19-25-18_ABQ-RVE-generation-and-analysis_csvfileslist.csv -f sweepL28.xlsx

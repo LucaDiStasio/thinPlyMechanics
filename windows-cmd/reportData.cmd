c44f90443885f60ba6572f1fdd48b54c6ab30c07
Luca Di Stasio <luca.di.stasio@ltu.se>
2018-03-14 11:51:03 +0100

    commit on 14/03/2018 at 11:51:03

--- windows-cmd/reportData.cmd
+++ windows-cmd/reportData.cmd
@@ -1,1 +1,1 @@
-cd D:/01*/07*/03*/Curved* && python D:/01_Luca/06_WD/thinPlyMechanics/python/reportData.py -w D:/OneDrive/01_Luca/07_DocMASE/07_Data/03_FEM/sweepOverDeltathetaL1_0992 -i 2018-03-13_18-04-00_ABQ-RVE-generation-and-analysis_csvfileslist.csv -f secondSweepL1_0992.xlsx --excel
+cd D:/01*/07*/03*/Curved* && python D:/01_Luca/06_WD/thinPlyMechanics/python/reportData.py -w D:/OneDrive/01_Luca/07_DocMASE/07_Data/03_FEM/sweepOverDeltathetaL1_0992 -i 2018-03-08_19-26-07_ABQ-RVE-generation-and-analysis_csvfileslist.csv -f secondSweepL1_0992.xlsx --excel

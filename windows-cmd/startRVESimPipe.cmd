4e401381dce58bdc58133a6a3ca74a7eb20c00c7
LucaDiStasio <luca.distasio@gmail.com>
2018-03-24 15:29:08 +0100

    commit on 24/03/2018 at 15:29:08

--- windows-cmd/startRVESimPipe.cmd
+++ windows-cmd/startRVESimPipe.cmd
@@ -1,1 +1,1 @@
-cd D:/01*/07*/03*/Curved* && abaqus CAE noGUI=D:/01_Luca/06_WD/thinPlyMechanics/python/createAndAnalyzeRVEs.py -- -dir D:/OneDrive/01_Luca/07_DocMASE/07_Data/03_FEM/InputData -data inputRVEdataL1_144.deck -iterables inputRVEiterablesL1_144.deck -plot inputRVEplot
+cd D:/01*/07*/03*/Curved* && abaqus CAE noGUI=D:/01_Luca/06_WD/thinPlyMechanics/python/createAndAnalyzeRVEs.py -- -dir D:/OneDrive/01_Luca/07_DocMASE/07_Data/03_FEM/InputData -data inputRVEdataL1_25.deck -iterables inputRVEiterablesL1_25.deck -plot inputRVEplot

b2dd3e2523081541ec3a9d3363ac143ecb68982f
LucaDiStasio <luca.distasio@gmail.com>
2018-05-15 18:36:47 +0200

    fixes #34

--- python/createAndAnalyzeRVEs.py
+++ python/createAndAnalyzeRVEs.py
@@ -3947,8 +3947,8 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
 
     if 'boundingPly' in parameters['BC']['northSide']['type']:
         model.FieldOutputRequest(name='F-Output-1',createStepName='Load-Step',region=model.rootAssembly.instances['RVE-assembly'].sets['MAIN-PLY'],variables=('U','RF','S','E','EE','COORD',))
-        model.FieldOutputRequest(name='F-Output-1',createStepName='Load-Step',region=model.rootAssembly.instances['RVE-assembly'].sets['RIGHTSIDE'],variables=('U','S'))
-        model.FieldOutputRequest(name='F-Output-1',createStepName='Load-Step',region=model.rootAssembly.instances['RVE-assembly'].sets['LEFTSIDE'],variables=('U','S'))
+        model.FieldOutputRequest(name='F-Output-1',createStepName='Load-Step',region=model.rootAssembly.instances['RVE-assembly'].sets['RIGHTSIDE'],variables=('COORD','S'))
+        model.FieldOutputRequest(name='F-Output-1',createStepName='Load-Step',region=model.rootAssembly.instances['RVE-assembly'].sets['LEFTSIDE'],variables=('COORD','S'))
     else:
         model.FieldOutputRequest(name='F-Output-1',createStepName='Load-Step',variables=('U','RF','S','E','EE','COORD',))
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + '... done.',True)

976d1f55b33bc37e547e75cc63ea4d3aed0ba706
LucaDiStasio <luca.distasio@gmail.com>
2018-05-16 18:37:02 +0200

    commit on 16/05/2018 at 18:37:02

--- ansys-APDL/transverseCrack.cmd
+++ ansys-APDL/transverseCrack.cmd
@@ -9,7 +9,18 @@
 L = 1       ! [mm] length of the element
 t = 1       ! [mm] 2t = thickness of the element
 tRatio = 1  ! [-]  ratio of bounding ply thickness to main ply
+a = 0.5     ! [mm] 2a = crack length
+w = 0.1     ! [mm] width of refined mesh region along the crack
+
+tBPly = tRatio*(2*t)
+tTotal = t + tBPly
 
 ! Create Geometry
 
-BLC4, 0.0, 0.0, L, t
+BLC4, 0.0, 0.0, L, tTotal
+
+K, 1, 0.0, a
+K, 2, w, 0.0
+K, 3, w, t
+K, 4, 0.0, t
+K, 5, L, t

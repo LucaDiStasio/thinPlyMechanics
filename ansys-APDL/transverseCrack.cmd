9f31dc1516b5b64b19a9abe4c3f0ff207cc24e92
LucaDiStasio <luca.distasio@gmail.com>
2018-05-16 19:14:46 +0200

    commit on 16/05/2018 at 19:14:45

--- ansys-APDL/transverseCrack.cmd
+++ ansys-APDL/transverseCrack.cmd
@@ -6,11 +6,20 @@
 
 ! Parameters
 
+Vf = ! [-] Fiber volume fraction
+
 L = 1       ! [mm] length of the element
 t = 1       ! [mm] 2t = thickness of the element
 tRatio = 1  ! [-]  ratio of bounding ply thickness to main ply
 a = 0.5     ! [mm] 2a = crack length
 w = 0.1     ! [mm] width of refined mesh region along the crack
+
+EL = ! [MPa] UD longitudinal Young's modulus
+ET = ! [MPa] UD transverse Young's modulus
+nuLT = ! [-] UD in-plane Poisson ratio
+nuTT = ! [-] UD transverse Poisson ratio
+GL = ! [MPa] UD in-plane shear modulus
+GT = ! [MPa] UD transverse shear modulus
 
 tBPly = tRatio*(2*t)
 tTotal = t + tBPly
@@ -35,8 +44,24 @@ KEYOPT,1,3,3		! Plane stress element with thickness
 
 ! Define Material Properties
 
-MP,EX,1,200000        ! mp,Young's modulus,material number,value
-MP,PRXY,1,0.3         ! mp,Poisson's ratio,material number,value
+MP,EX,1,ET        ! 1 is cross-ply, 2 is ud-ply 
+MP,EY,1,ET        ! 1 is cross-ply, 2 is ud-ply
+MP,EZ,1,EL        ! 1 is cross-ply, 2 is ud-ply
+MP,NUXY,1,nuTT    ! mp,Poisson's ratio,material number,value
+MP,NUYZ,1,nuLT    ! mp,Poisson's ratio,material number,value
+MP,NUXZ,1,nuLT    ! mp,Poisson's ratio,material number,value
+MP,GXY,1,GTT      ! mp,Poisson's ratio,material number,value
+MP,GYZ,1,GLT      ! mp,Poisson's ratio,material number,value
+MP,GXZ,1,GLT      ! mp,Poisson's ratio,material number,value
+MP,EX,2,EL        ! 1 is cross-ply, 2 is ud-ply 
+MP,EY,2,ET        ! 1 is cross-ply, 2 is ud-ply
+MP,EZ,2,ET        ! 1 is cross-ply, 2 is ud-ply
+MP,NUXY,2,nuLT    ! mp,Poisson's ratio,material number,value
+MP,NUYZ,2,nuTT    ! mp,Poisson's ratio,material number,value
+MP,NUXZ,2,nuLT    ! mp,Poisson's ratio,material number,value
+MP,GXY,2,GLT      ! mp,Poisson's ratio,material number,value
+MP,GYZ,2,GTT      ! mp,Poisson's ratio,material number,value
+MP,GXZ,2,GLT      ! mp,Poisson's ratio,material number,value
 
 ! Define the number of elements each line is to be divided into
 AESIZE,ALL,5    	  ! lesize,all areas,size of element

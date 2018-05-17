e9abbd845ee7cd69736f595fda04c27d566b2e40
Luca Di Stasio <luca.di.stasio@ltu.se>
2018-05-17 11:41:07 +0200

    commit on 17/05/2018 at 11:41:07

--- python/interpolateERRs.py
+++ python/interpolateERRs.py
@@ -41,19 +41,6 @@ from os.path import isfile, join, exists
 from openpyxl import load_workbook
 
 def readData(wd,workbook):
-    data = {}
-    boundaryCase = ['free','geomcoupling','fixedv','fixedvlinearu']
-    Vf = [0.000079,0.0001,0.2,0.3,0.4,0.5,0.55,0.6,0.65]
-    theta = [10.0,20.0,30.0,40.0,50.0,60.0,70.0,80.0,90.0,100.0,110.0,120.0,130.0,140.0,150.0]
-    for case in boundaryCase:
-        for v,value in enumerate(Vf):
-            data['GI']['VCCT'][case][v+1] = {'Vf':value,'theta':theta,'values':[]}
-            data['GI']['Jint'][case][v+1] = {'Vf':value,'theta':theta,'values':[]}
-            data['GII'][case][v+1] = {'Vf':value,'theta':theta,'values':[]}
-            data['GTOT']['VCCT'][case][v+1] = {'Vf':value,'theta':theta,'values':[]}
-            data['GTOT']['Jint'][case][v+1] = {'Vf':value,'theta':theta,'values':[]}
-            data['CZ'][case][v+1] = {'Vf':value,'theta':theta,'values':[]}
-            data['SZ'][case][v+1] = {'Vf':value,'theta':theta,'values':[]}
     wb = load_workbook(filename=join(wd,workbook), read_only=True)
     gIvcctWorksheet = wb['GI-VCCT']
     gIjintWorksheet = wb['GI-Jint']
@@ -62,12 +49,45 @@ def readData(wd,workbook):
     gTOTjintWorksheet = wb['GTOT-Jint']
     czWorksheet = wb['ContactZone']
     szWorksheet = wb['SeparationZone']
-
+    data = {}
+    boundaryCase = ['free','geomcoupling','fixedv','fixedvlinearu']
+    Vf = [0.000079,0.0001,0.2,0.3,0.4,0.5,0.55,0.6,0.65]
+    Vfcolumns = {0.000079:'F',0.0001:'G',0.2:'H',0.3:'I',0.4:'J',0.5:'K',0.55:'L',0.6:'M',0.65:'N'}
+    theta = [10.0,20.0,30.0,40.0,50.0,60.0,70.0,80.0,90.0,100.0,110.0,120.0,130.0,140.0,150.0]
+    nTheta = len(theta)
+    initTheta = 4
+    for c,case in enumerate(boundaryCase):
+        for v,value in enumerate(Vf):
+            values = []
+            for n in range(0,nTheta):
+                values.append(gIvcctWorksheet[Vfcolumns[value]+str(4+c*nTheta+n)])
+            data['GI']['VCCT'][case][v+1] = {'Vf':value,'theta':theta,'values':values}
+            values = []
+            for n in range(0,nTheta):
+                values.append(gIjintWorksheet[Vfcolumns[value]+str(4+c*nTheta+n)])
+            data['GI']['Jint'][case][v+1] = {'Vf':value,'theta':theta,'values':values}
+            values = []
+            for n in range(0,nTheta):
+                values.append(gIIvcctWorksheet[Vfcolumns[value]+str(4+c*nTheta+n)])
+            data['GII'][case][v+1] = {'Vf':value,'theta':theta,'values':values}
+            values = []
+            for n in range(0,nTheta):
+                values.append(gTOTvcctWorksheet[Vfcolumns[value]+str(4+c*nTheta+n)])
+            data['GTOT']['VCCT'][case][v+1] = {'Vf':value,'theta':theta,'values':values}
+            values = []
+            for n in range(0,nTheta):
+                values.append(gTOTjintWorksheet[Vfcolumns[value]+str(4+c*nTheta+n)])
+            data['GTOT']['Jint'][case][v+1] = {'Vf':value,'theta':theta,'values':values}
+            values = []
+            for n in range(0,nTheta):
+                values.append(czWorksheet[Vfcolumns[value]+str(4+c*nTheta+n)])
+            data['CZ'][case][v+1] = {'Vf':value,'theta':theta,'values':values}
+            values = []
+            for n in range(0,nTheta):
+                values.append(szWorksheet[Vfcolumns[value]+str(4+c*nTheta+n)])
+            data['SZ'][case][v+1] = {'Vf':value,'theta':theta,'values':values}
     return data
 
-for row in ws.rows:
-    for cell in row:
-        print(cell.value)
 
 
 

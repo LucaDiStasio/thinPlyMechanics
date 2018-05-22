f6f3c1a4639e6eae081ef1004c7f00bbc06dc39f
Luca Di Stasio <luca.di.stasio@ltu.se>
2018-05-22 14:16:09 +0200

    commit on 22/05/2018 at 14:16:09

--- python/createAndAnalyzeRVEs.py
+++ python/createAndAnalyzeRVEs.py
@@ -5904,6 +5904,7 @@ def analyzeRVEresults(odbname,parameters,logfilepath,baselogindent,logindent):
     for load in parameters['loads'].values():
         if ('appliedUniformPressure' in load['type'] or 'applieduniformpressure' in load['type'] or 'applied Uniform Pressure' in load['type'] or 'applied uniform pressure' in load['type']) and 'Load-Step' in load['stepName'] and 'CRACK' in load['set']:
             isPressureLoadedCrack = True
+            uniformP = load['value']
             writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + 'Pressure loaded crack faces are present, corrected VCCT will be used.',True)
             break
     if not isPressureLoadedCrack:
@@ -5933,6 +5934,9 @@ def analyzeRVEresults(odbname,parameters,logfilepath,baselogindent,logindent):
         yRFfirstbounded = RFfirstbounded.values[0].data[1]
         rRFfirstbounded = np.cos(phi)*xRFfirstbounded + np.sin(phi)*yRFfirstbounded
         thetaRFfirstbounded = -np.sin(phi)*xRFfirstbounded + np.cos(phi)*yRFfirstbounded
+        if isPressureLoadedCrack:
+            rRFcracktip -= /6
+            rRFfirstbounded -= 2*parameters['']/3
 
     xfiberCracktipDisplacement = fiberCracktipDisplacement.values[0].data[0]
     yfiberCracktipDisplacement = fiberCracktipDisplacement.values[0].data[1]

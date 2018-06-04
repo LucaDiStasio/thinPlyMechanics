4ea73da2caabe8efa8e72dfc7eb06f4c54b17d48
LucaDiStasio <luca.distasio@gmail.com>
2018-06-04 22:49:52 +0200

    fixes #48, commit on 04/06/2018 at 22:49:51

--- python/createAndAnalyzeRVEs.py
+++ python/createAndAnalyzeRVEs.py
@@ -3445,6 +3445,9 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
 
     # sets of edges
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + 'Sets of edges',True)
+    alpha = theta + deltatheta - deltapsi
+    beta = theta + deltatheta + deltapsi
+    gamma = theta + deltatheta + deltapsi + deltaphi
     setsOfEdgesData = [[0.99*Rf*np.cos(0.5*alpha*np.pi/180),0.99*Rf*np.sin(0.5*alpha*np.pi/180),0.0,1.01*Rf*np.cos(0.5*alpha*np.pi/180),1.01*Rf*np.sin(0.5*alpha*np.pi/180),0.0,'CRACK-LOWER'],
  [0.99*Rf*np.cos((alpha+0.5*deltapsi)*np.pi/180),0.99*Rf*np.sin((alpha+0.5*deltapsi)*np.pi/180),0.0,1.01*Rf*np.cos((alpha+0.5*deltapsi)*np.pi/180),1.01*Rf*np.sin((alpha+0.5*deltapsi)*np.pi/180),0.0,'CRACK-UPPER']]
     for setOfEdgesData in setsOfEdgesData:
@@ -3610,7 +3613,12 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
                        [1.05*Rf*np.cos(0.99*gamma*np.pi/180),1.05*Rf*np.sin(0.99*gamma*np.pi/180),0.0,1.05*Rf*np.cos(1.01*gamma*np.pi/180),1.05*Rf*np.sin(1.01*gamma*np.pi/180),0.0,'TRANSVERSALCUT-FOURTHMATRIX']]
     for setOfEdgesData in setsOfEdgesData:
         defineSetOfEdgesByClosestPoints(RVEpart,setOfEdgesData[0],setOfEdgesData[1],setOfEdgesData[2],setOfEdgesData[3],setOfEdgesData[4],setOfEdgesData[5],setOfEdgesData[-1],logfilepath,baselogindent + 4*logindent,True)
-
+    
+    if np.abs(theta)>0.0:
+        alpha = theta - deltatheta + deltapsi
+        beta = theta - deltatheta - deltapsi
+        gamma = theta - deltatheta - deltapsi - deltaphi
+    
     # sets of faces
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + 'Sets of faces',True)
 

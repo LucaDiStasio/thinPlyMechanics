6cda03d4b95ff4c663e46b566421b6218623f478
LucaDiStasio <luca.distasio@gmail.com>
2018-06-05 19:57:28 +0200

    commit on 05/06/2018 at 19:57:28

--- python/createAndAnalyzeRVEs.py
+++ python/createAndAnalyzeRVEs.py
@@ -3021,14 +3021,15 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
         CornerBx = L
     theta = parameters['geometry']['theta'] # in degrees !!!
     deltatheta = parameters['geometry']['deltatheta'] # in degrees !!!
-    if theta-deltatheta<=0.0:
-        skipLineToLogFile(logfilepath,'a',True)
-        writeErrorToLogFile(logfilepath,'a','GEOMETRY','The provided debond geometry is not correct: the debond ends at or under the symmetry line at 0 degree',True)
-        sys.exit(2)
-    elif theta+deltatheta>=180.0:
-        skipLineToLogFile(logfilepath,'a',True)
-        writeErrorToLogFile(logfilepath,'a','GEOMETRY','The provided debond geometry is not correct: the debond ends at or under the symmetry line at 180 degree',True)
-        sys.exit(2)
+    if np.abs(theta)>0.0:
+        if theta-deltatheta<=0.0:
+            skipLineToLogFile(logfilepath,'a',True)
+            writeErrorToLogFile(logfilepath,'a','GEOMETRY','The provided debond geometry is not correct: the debond ends at or under the symmetry line at 0 degree',True)
+            sys.exit(2)
+        elif theta+deltatheta>=180.0:
+            skipLineToLogFile(logfilepath,'a',True)
+            writeErrorToLogFile(logfilepath,'a','GEOMETRY','The provided debond geometry is not correct: the debond ends at or under the symmetry line at 180 degree',True)
+            sys.exit(2)
     deltapsi = parameters['mesh']['size']['deltapsi'] # in degrees !!!
     deltaphi = parameters['mesh']['size']['deltaphi'] # in degrees !!!
     delta = parameters['mesh']['size']['delta'] # in degrees !!!

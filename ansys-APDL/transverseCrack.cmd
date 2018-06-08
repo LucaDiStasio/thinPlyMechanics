8502acee6ccd42c5812d7b12dcd3bede719ab3fd
LucaDiStasio <luca.distasio@gmail.com>
2018-06-08 22:37:08 +0200

    commit on 08/06/2018 at 22:37:07

--- ansys-APDL/transverseCrack.cmd
+++ ansys-APDL/transverseCrack.cmd
@@ -275,6 +275,8 @@ SAVE                 ! Save your work to the database
 
 /POST1               ! Post processing
 
+PRCINT, 2, , JINT
+
 ALLSEL
 
 *GET,NNodes,NODE,0,COUNT                  !Get the number of nodes in the selected set

44d79c2e2ad16f27dd361a4fe95c82a58010ac6e
LucaDiStasio <luca.distasio@gmail.com>
2018-06-09 12:41:16 +0200

    commit on 09/06/2018 at 12:41:16

--- python/reportData.py
+++ python/reportData.py
@@ -453,11 +453,7 @@ def main(argv):
                     except Exception,error:
                         print('        '+str(error))
                         worksheet.write(c+1,e,str(element).decode('utf-8'),numberFormat)
-                        #sys.exc_clear()
-                        if  'NAN' in str(error) or 'INF' in str(error):
-                            sys.exit(2)
-                        else:
-                            sys.exc_clear()
+                        sys.exc_clear()
             for p,plot in enumerate(plotSettings):
                 print('        Create plot ' + plot[-1] + ' in sheet ' + sheetName)
                 chart = workbook.add_chart({'type': 'scatter',

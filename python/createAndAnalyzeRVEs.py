7c5ee8859c88e16d2f0735453a7b43910a95922e
LucaDiStasio <luca.distasio@gmail.com>
2018-09-03 19:27:32 +0200

    correction to account for size with many fibers; commit on 03/09/2018 at 19:27:32

--- python/createAndAnalyzeRVEs.py
+++ python/createAndAnalyzeRVEs.py
@@ -3000,6 +3000,10 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
     else:
         CornerBy = L
     if 'boundingPly' in parameters['BC']['rightSide']['type'] and 'boundingPly' in parameters['BC']['leftSide']['type'] and 'adjacentFibers' in parameters['BC']['rightSide']['type'] and 'adjacentFibers' in parameters['BC']['leftSide']['type']:
+        if 'quarter' in parameters['geometry']['fiber']['type']:
+            skipLineToLogFile(logfilepath,'a',True)
+            writeErrorToLogFile(logfilepath,'a','GEOMETRY','Clashing geometric requirements: asked for quarter fiber and for material on the left side. Review and select the appropriate.',True)
+            sys.exit(2)
         wRatioRight = parameters['BC']['rightSide']['wRatio']
         wRatioLeft = parameters['BC']['leftSide']['wRatio']
         nFibersRight = parameters['BC']['rightSide']['nFibers']

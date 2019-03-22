b399b113a4459330b0dbe8e7acab27a0319f11fb
Luca Di Stasio <luca.di.stasio@ltu.se>
2019-03-22 18:39:07 +0100

    commit on 22/03/2019 at 18:39:07

--- python/createAndAnalyzeMultipleVerticalDebondRVEs.py
+++ python/createAndAnalyzeMultipleVerticalDebondRVEs.py
@@ -4374,8 +4374,8 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
 
     for nDebond in range(0,nDebonds):
         model.rootAssembly.engineeringFeatures.assignSeam(regions=model.rootAssembly.instances['RVE-assembly'].sets['DEBFIBER-N'+str(nDebond+1)+'-DEBONDEDINTERFACE'])
-        masterSurface = model.rootAssembly.Surface(side1Edges=model.rootAssembly.instances['RVE-assembly'].sets['DEBFIBER-N'+str(nDebond+1)+'-DEBONDEDINTERFACE'],name='DEBFIBER-N'+str(nDebond+1)+'-MASTERSURFACE')
-        slaveSurface = model.rootAssembly.Surface(side2Edges=model.rootAssembly.instances['RVE-assembly'].sets['DEBFIBER-N'+str(nDebond+1)+'-DEBONDEDINTERFACE'],name='DEBFIBER-N'+str(nDebond+1)+'-SLAVESURFACE')
+        masterSurface = model.rootAssembly.Surface(side1Edges=model.rootAssembly.instances['RVE-assembly'].sets['DEBFIBER-N'+str(nDebond+1)+'-DEBONDEDINTERFACE'].edges,name='DEBFIBER-N'+str(nDebond+1)+'-MASTERSURFACE')
+        slaveSurface = model.rootAssembly.Surface(side2Edges=model.rootAssembly.instances['RVE-assembly'].sets['DEBFIBER-N'+str(nDebond+1)+'-DEBONDEDINTERFACE'].edges,name='DEBFIBER-N'+str(nDebond+1)+'-SLAVESURFACE')
         mdb.models['Model-1'].SurfaceToSurfaceContactStd(name='DEBFIBER-N'+str(nDebond+1)+'-CONTACTINTERACTION',createStepName='Initial',master=masterSurface,slave=slaveSurface,sliding=SMALL,interactionProperty='PARTIALLYDEBONDEDFIBERS')
 
     if 'inverseSquareRoot' in parameters['singularity']['type']:

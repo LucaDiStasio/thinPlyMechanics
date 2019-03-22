13740f092d9ff6c72d9c34afec739c5c7eaeb507
Luca Di Stasio <luca.di.stasio@ltu.se>
2019-03-22 12:42:24 +0100

    commit on 22/03/2019 at 12:42:24

--- python/createAndAnalyzeMultipleVerticalDebondRVEs.py
+++ python/createAndAnalyzeMultipleVerticalDebondRVEs.py
@@ -3478,11 +3478,17 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
         fiberSketch.CircleByCenterPerimeter(center=(0.0, 0.0+(nDebond+1)*2*L), point1=(Rf, 0.0+(nDebond+1)*2*L))
         deltathetaDebond = parameters['geometry']['debonds']['deltatheta'][nDebond]
         thetaDebond = parameters['geometry']['debonds']['theta'][nDebond]
-        xA = Rf*np.cos((thetaDebond+deltathetaDebond)*np.pi/180.0)
-        yA = Rf*np.sin((thetaDebond+deltathetaDebond)*np.pi/180.0)
-        xB = Rf*np.cos((thetaDebond-deltathetaDebond)*np.pi/180.0)
-        yB = Rf*np.sin((thetaDebond-deltathetaDebond)*np.pi/180.0)
+        xA = 0.95*Rf*np.cos((thetaDebond+deltathetaDebond)*np.pi/180.0)
+        yA = 0.95*Rf*np.sin((thetaDebond+deltathetaDebond)*np.pi/180.0)
+        xB = 0.95*Rf*np.cos((thetaDebond-deltathetaDebond)*np.pi/180.0)
+        yB = 0.95*Rf*np.sin((thetaDebond-deltathetaDebond)*np.pi/180.0)
+        xC = 1.05*Rf*np.cos((thetaDebond+deltathetaDebond)*np.pi/180.0)
+        yC = 1.05*Rf*np.sin((thetaDebond+deltathetaDebond)*np.pi/180.0)
+        xD = 1.05*Rf*np.cos((thetaDebond-deltathetaDebond)*np.pi/180.0)
+        yD = 1.05*Rf*np.sin((thetaDebond-deltathetaDebond)*np.pi/180.0)
         fiberSketch.ArcByCenterEnds(center=(0.0, 0.0+(nDebond+1)*2*L), point1=(0.0+xA, 0.0+(nDebond+1)*2*L+yA), point2=(0.0+xB, 0.0+(nDebond+1)*2*L+yB), direction=CLOCKWISE)
+        fiberSketch.Line(point1=(Ax,Ay),point2=(Bx,By))
+        fiberSketch.Line(point1=(Ax,Ay),point2=(Bx,By))
     listGeomElements(logfilepath,baselogindent+2*logindent,logindent,fiberGeometry,fiberVertices)
     writeLineToLogFile(logfilepath,'a',baselogindent + 2*logindent + '... done.',True)
     # if bounding ply is present, draw interface line

c0c4c3fb183d90c19073e3a6b5f606faf25e454c
Luca Di Stasio <luca.di.stasio@ltu.se>
2019-03-25 14:28:03 +0100

    commit on 25/03/2019 at 14:28:03

--- python/reportData.py
+++ python/reportData.py
@@ -833,7 +833,11 @@ def main(argv):
                     radialpathsSheetnames.append(datasheetName)
                     numberOfRadialpaths.append(len(pathVariables))
                     worksheet = radialpathsWorkbook.add_worksheet(datasheetName.decode('utf-8'))
+                    print('    --> Writing worksheet')
+                    print('        ' + datasheetName)
+                    print(' ')
                     for p, pathVariable in enumerate(pathVariables):
+                        print('          pathAngle = ' + str(pathVariable) + ' deg')
                         worksheet.write(0,p*25,pathVariableName,stringFormat)
                         worksheet.write(1,p*25,pathVariable,radialpathsnumberFormatReduced)
                         worksheet.write(0,p*25+1,pathStartVariableName,stringFormat)
@@ -865,6 +869,7 @@ def main(argv):
                         worksheet.write(2,p*25+22,'I3_3D [MPa^3]',stringFormat)
                         worksheet.write(2,p*25+23,'I1_2D [MPa]',stringFormat)
                         worksheet.write(2,p*25+24,'I2_2D [MPa^2]',stringFormat)
+                        print('          number of path points = ' + str(pathCoords[p]))
                         for c,coord in enumerate(pathCoords[p]):
                             worksheet.write(3+c,p*25,coord,radialpathsnumberFormat)
                             worksheet.write(3+c,p*25+1,pathNormCoords[p][c],radialpathsnumberFormat)

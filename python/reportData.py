11808fd43fd6685ae11b737875ec0557228383b3
Luca Di Stasio <luca.di.stasio@ltu.se>
2019-03-25 13:16:43 +0100

    commit on 25/03/2019 at 13:16:43

--- python/reportData.py
+++ python/reportData.py
@@ -414,8 +414,10 @@ def main(argv):
     print('Extracting names of subfolders ...')
     try:
         subfoldersList = []
-        for line in lines[1:]:
-            subfoldersList.append('/'.join(line.replace('\n','').split(',')[0].replace('\\','/').split('/')[:-1]))
+        for l,line in enumerate(lines[1:]):
+            currentSubfolder = '/'.join(line.replace('\n','').split(',')[0].replace('\\','/').split('/')[:-1])
+            print('  ' + str(l+1) + '. ' + str(currentSubfolder))
+            subfoldersList.append(currentSubfolder)
         print('...done.')
     except Exception,error:
         print('EXCEPTION ENCOUNTERED')

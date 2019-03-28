fb6e494d4bd3ac88c5b4242d7d32d21e3e1eee29
Luca Di Stasio <luca.di.stasio@ltu.se>
2019-03-28 12:36:13 +0100

    commit on 28/03/2019 at 12:36:13

--- python/createAndAnalyzeStiffnessRVEs.py
+++ python/createAndAnalyzeStiffnessRVEs.py
@@ -3817,9 +3817,9 @@ def createRVE(parameters,logfilepath,baselogindent,logindent):
     # assign seam
     model.rootAssembly.engineeringFeatures.assignSeam(regions=model.rootAssembly.instances['RVE-assembly'].sets['CRACK'])
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + '-- Seam assigned',True)
-    masterSurface = model.rootAssembly.Surface(side1Edges=model.rootAssembly.instances['RVE-assembly'].sets['CRACK'].edges, name='MASTER')
+    masterSurface = model.rootAssembly.Surface(side1Edges=model.rootAssembly.instances['RVE-assembly'].sets['CRACK'].edges, name='FIBER-SURFACE')
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + '-- Master surface created',True)
-    slaveSurface = model.rootAssembly.Surface(side2Edges=model.rootAssembly.instances['RVE-assembly'].sets['CRACK'].edges,name='SLAVE')
+    slaveSurface = model.rootAssembly.Surface(side2Edges=model.rootAssembly.instances['RVE-assembly'].sets['CRACK'].edges,name='MATRIX-SURFACE')
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + '--Slave surface created',True)
     model.SurfaceToSurfaceContactStd(name='CRACK-CONTACTINTERACTION',createStepName='Initial',master=masterSurface,slave=slaveSurface,sliding=SMALL,interactionProperty='PARTIALLYDEBONDEDFIBERS')
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + '-- Contact interaction created',True)
@@ -4643,8 +4643,9 @@ def analyzeRVEresults(odbname,parameters,logfilepath,baselogindent,logindent):
     #=======================================================================
     writeLineToLogFile(logfilepath,'a',baselogindent + 2*logindent + 'Compute average COD and CSD ...',True)
 
-    writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + 'Select crack faces ...',True)
-    rveEls = odb.rootAssembly.instances['RVE-ASSEMBLY'].surfaces['']
+    writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + 'Extract nodes belonging to crack faces ...',True)
+    fibersurfaceNodes = odb.rootAssembly.instances['RVE-ASSEMBLY'].surfaces['FIBER-SURFACE'].nodes
+    matrixsurfaceNodes = odb.rootAssembly.instances['RVE-ASSEMBLY'].surfaces['MATRIX-SURFACE'].nodes
     writeLineToLogFile(logfilepath,'a',baselogindent + 3*logindent + '... done.',True)
 
     #=======================================================================

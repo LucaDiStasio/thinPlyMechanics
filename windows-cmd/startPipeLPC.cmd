633976f499418e84287e185038011ca208a48d23
Luca Di Stasio <luca.distasio@gmail.com>
2019-06-10 18:03:01 +0200

    commit on 10/06/2019 at 18:02:59

--- windows-cmd/startPipeLPC.cmd
+++ windows-cmd/startPipeLPC.cmd
@@ -1,1 +1,1 @@
-cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startERVESimPipeLPC.cmd 1_144S0A10T1 && cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startERVESimPipeLPC.cmd 1_144S0A100 && cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startERVESimPipeLPC.cmd 1_144S0A100T1
+cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startERVESimPipeLPC.cmd 1_144S5A5 && cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startERVESimPipeLPC.cmd 1_144S5A5T1

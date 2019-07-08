9fc4ffe41caa66d7e87c0aa86adb35e62f96dee4
Luca Di Stasio <luca.distasio@gmail.com>
2019-07-08 15:19:20 +0200

    commit on 08/07/2019 at 15:19:11

---
+++ windows-cmd/startPipeElTypeLPC.cmd
@@ -0,0 +1,1 @@
+cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startElTypeRVESimPipeLPC.cmd 1_144S0A0
---
+++ windows-cmd/startPipeStiffMatrixLPC.cmd
@@ -0,0 +1,1 @@
+cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startEmatrixRVESimPipeLPC.cmd 1_144S0A0

f1f85935c312ef677032761a42f9312758587f2b
Luca Di Stasio <luca.distasio@gmail.com>
2019-07-10 17:24:20 +0200

    commit on 10/07/2019 at 17:24:20

--- windows-cmd/startPipeElTypeLPC.cmd
+++ windows-cmd/startPipeElTypeLPC.cmd
@@ -1,1 +1,1 @@
-cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startElTypeRVESimPipeLPC.cmd FreeL1_144S0A0
+cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startElTypeRVESimPipeLPC.cmd FreeL1_144S0A0 && cd C:/02*/01*/01*/thinPlyMechanics/windows-cmd && startElTypeRVESimPipeLPC.cmd FreeL1_144S5A0

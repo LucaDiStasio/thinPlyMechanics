ffa1e079b5d6a411910570b71d3fbfd298f2ee5b
Luca Di Stasio <luca.distasio@gmail.com>
2019-09-11 21:54:55 +0200

    commit on 11/09/2019 at 21:54:53

--- tex/07_Thesis/make.bat
+++ tex/07_Thesis/make.bat
@@ -1,9 +1,9 @@
-latex thesis.tex
+pdflatex thesis.tex
 bibtex bu1
 bibtex bu2
 bibtex bu3
 bibtex bu4
-latex thesis.tex
-latex thesis.tex
+pdflatex thesis.tex
+pdflatex thesis.tex
 pdflatex thesis.tex
 pause
